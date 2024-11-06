74c91ac37092a1ca20a027ff8bedb08fc9ea4a26
Blxee <aminedev57@gmail.com>
2024-11-06 16:02:13 +0100

    Changed: chess board is visually sound

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{array, fmt};
+use std::{fmt, io::stdin};
 
 const WIDTH: usize = 8;
 const HEIGHT: usize = 8;
@@ -8,14 +8,12 @@ struct ChessBoard {
 }
 
 enum ChessColor {
-    White,
-    Black,
+    WHITE,
+    BLACK,
 }
 use ChessColor::*;
 
-#[derive(Default)]
 enum ChessCell {
-    #[default]
     Empty,
     Filled(ChessPiece),
 }
@@ -38,20 +36,29 @@ enum PieceType {
 use PieceType::*;
 
 impl ChessBoard {
-    fn new() -> Self {
+    const fn new() -> Self {
         let mut grid = [const { [const { ChessCell::Empty }; WIDTH] }; HEIGHT];
-        grid[6] = [const {
-            Filled(ChessPiece {
-                piece_type: PAWN,
-                color: Black,
-            })
-        }; 8];
-        grid[1] = [const {
-            Filled(ChessPiece {
-                piece_type: PAWN,
-                color: White,
-            })
-        }; 8];
+
+        grid[7][0] = Filled(ChessPiece::new(ROOK, BLACK));
+        grid[7][1] = Filled(ChessPiece::new(KNIGHT, BLACK));
+        grid[7][2] = Filled(ChessPiece::new(BISHOP, BLACK));
+        grid[7][3] = Filled(ChessPiece::new(QUEEN, BLACK));
+        grid[7][4] = Filled(ChessPiece::new(KING, BLACK));
+        grid[7][5] = Filled(ChessPiece::new(BISHOP, BLACK));
+        grid[7][6] = Filled(ChessPiece::new(KNIGHT, BLACK));
+        grid[7][7] = Filled(ChessPiece::new(ROOK, BLACK));
+        grid[6] = [const { Filled(ChessPiece::new(PAWN, BLACK)) }; 8];
+
+        grid[1] = [const { Filled(ChessPiece::new(PAWN, WHITE)) }; 8];
+        grid[0][0] = Filled(ChessPiece::new(ROOK, WHITE));
+        grid[0][1] = Filled(ChessPiece::new(KNIGHT, WHITE));
+        grid[0][2] = Filled(ChessPiece::new(BISHOP, WHITE));
+        grid[0][3] = Filled(ChessPiece::new(QUEEN, WHITE));
+        grid[0][4] = Filled(ChessPiece::new(KING, WHITE));
+        grid[0][5] = Filled(ChessPiece::new(BISHOP, WHITE));
+        grid[0][6] = Filled(ChessPiece::new(KNIGHT, WHITE));
+        grid[0][7] = Filled(ChessPiece::new(ROOK, WHITE));
+
         Self { grid }
     }
 }
@@ -60,6 +67,7 @@ impl fmt::Display for ChessBoard {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "   a b c d e f g h")?;
         writeln!(f, "  +---------------+")?;
+
         for (i, row) in self.grid.iter().rev().enumerate() {
             write!(f, "{} ", (HEIGHT - i))?;
             for cell in row {
@@ -67,8 +75,9 @@ impl fmt::Display for ChessBoard {
             }
             writeln!(f, "| {}", (HEIGHT - i))?;
         }
+
         writeln!(f, "  +---------------+")?;
-        writeln!(f, "   a b c d e f g h")
+        write!(f, "   a b c d e f g h")
     }
 }
 
@@ -81,16 +90,23 @@ impl fmt::Display for ChessCell {
     }
 }
 
+impl ChessPiece {
+    const fn new(piece_type: PieceType, color: ChessColor) -> Self {
+        Self { piece_type, color }
+    }
+}
+
 impl fmt::Display for ChessPiece {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         const WHITE_PIECE_REPR: &str = "KQBHRP";
         const BLACK_PIECE_REPR: &str = "kqbhrp";
+
         write!(
             f,
             "{}",
             match self.color {
-                White => WHITE_PIECE_REPR.chars().nth(self.piece_type as usize),
-                Black => BLACK_PIECE_REPR.chars().nth(self.piece_type as usize),
+                WHITE => WHITE_PIECE_REPR.chars().nth(self.piece_type as usize),
+                BLACK => BLACK_PIECE_REPR.chars().nth(self.piece_type as usize),
             }
             .unwrap()
         )
